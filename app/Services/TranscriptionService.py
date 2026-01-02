b6de44902b051c0a0eb001548949791946a88364
Neil <neil.rabia972972@gmail.com>
2026-01-02 11:44:53 +0100

    Ajustement au niveau des variable d'env

--- app/Services/TranscriptionService.py
+++ app/Services/TranscriptionService.py
@@ -2,20 +2,19 @@ import requests
 
 # Service pour gérer les interactions avec le service Whisper
 class TranscriptionService:
-    def __init__(self, URL : str):
-        self.URL = URL
+    def __init__(self, BASE_URL : str):
+        self.BASE_URL = BASE_URL
 
     # Fonction pour envoyer le fichier audio au service Whisper et obtenir la transcription
     def send_to_whisper_service(self, audio_file) -> str:
         files = {
             "audioFile": audio_file
         }
-        response = requests.post(self.URL, files=files)
+        response = requests.post(f"{self.BASE_URL}/BatchTranscriptionService", files=files)
         response.raise_for_status()
         return response.json().get("transcription")
-
     
     # Fonction pour annuler une transcription en cours (si supporté par le service Whisper
     def cancel_transcription(self, job_id: str) -> bool:
-        response = requests.post(f"{self.URL}/cancel", json={"job_id": job_id})
+        response = requests.post(f"{self.BASE_URL}/cancel", json={"job_id": job_id})
         return response.status_code == 200
